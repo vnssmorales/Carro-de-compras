4dd4669ce293682a6017e91311fe01a17cb2f031
vanemh <vnss.morales@gmail.com>
2023-05-06 19:06:09 -0400

    creando carro de compras

---
+++ src/assets/CartApp.jsx
@@ -0,0 +1,80 @@
+export const CartApp = () => {
+  return (
+    <>
+      <div className="container">
+        <h3>Cart App</h3>
+       
+        <div className="row">
+            
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Teclado mecanico RGB</h5>
+                <p className="card-text">Teclado mecánico con luces</p>
+                <p className="card-text">$1.000</p>
+                <button className="btn btn-primary">Agregar</button>
+              </div>
+            </div>
+            </div>
+
+            <div className="col-4 my-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Teclado mecanico RGB</h5>
+                  <p className="card-text">Teclado mecánico con luces</p>
+                  <p className="card-text">$1.000</p>
+                  <button className="btn btn-primary">Agregar</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4 my-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Teclado mecanico RGB</h5>
+                  <p className="card-text">Teclado mecánico con luces</p>
+                  <p className="card-text">$1.000</p>
+                  <button className="btn btn-primary">Agregar</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4 my-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Teclado mecanico RGB</h5>
+                  <p className="card-text">Teclado mecánico con luces</p>
+                  <p className="card-text">$1.000</p>
+                  <button className="btn btn-primary">Agregar</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4 my-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Teclado mecanico RGB</h5>
+                  <p className="card-text">Teclado mecánico con luces</p>
+                  <p className="card-text">$1.000</p>
+                  <button className="btn btn-primary">Agregar</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4 my-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Teclado mecanico RGB</h5>
+                  <p className="card-text">Teclado mecánico con luces</p>
+                  <p className="card-text">$1.000</p>
+                  <button className="btn btn-primary">Agregar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+       
+      
+    </>
+  );
+};
