7466953f492f3c2ce146892c8cda92432ce3950f
vanemh <vnss.morales@gmail.com>
2023-05-06 19:59:30 -0400

    creo la carpeta data y una funcion para obtener los productos

---
+++ src/data/products.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    name: "Teclado mecanico RGB",
+    description: "Teclado mecánico con luces",
+    price: 1000,
+  },
+  {
+    id: 2,
+    name: "Samsung Smart TV 55",
+    description: "Excelente TV LCD",
+    price: 3000,
+  },
+  {
+    id: 3,
+    name: "Audífono Bluetooh Sony",
+    description: "Audífono Bluetooh para smartphone",
+    price: 700,
+  },
+  {
+    id: 4,
+    name: "Memoria Corsair 8GB DDR5",
+    description: "Memoria RAM optimizada para juegos",
+    price: 3700,
+  },
+  {
+    id: 5,
+    name: "Asus Nvidia RTX",
+    description: "Tarjeta gráfica para juegos 4k",
+    price: 5000,
+  },
+  {
+    id: 6,
+    name: "CPU Intel Core I7",
+    description: "Optimizada para tareas multicore",
+    price: 4000,
+  },
+];
